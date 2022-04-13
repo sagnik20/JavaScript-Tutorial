e90285e7cd05646fb59b58acf8e932ea95d16140
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2022-04-13 08:14:39 +0530

    functions

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,18 @@
 console.log("Funtions tutorial");
 
-function greet(name,reply="Thanks a lot"){
+function greet(name,reply="Thanks a lot"){//reply has a default value;
+
+    console.log(`Happy Birthday ${name}.
+    Wow ${reply}`);
+
+    let mag = `Happy Birthday ${name}. i'm so happy for you. Enjoy Your day!
+    Wow ${reply} returned` 
+
+    return mag;
+}
+
+//Annonymous Function
+const mygreet = function(name,reply="Thanks a lot"){
 
     console.log(`Happy Birthday ${name}.
     Wow ${reply}`);
@@ -14,3 +26,18 @@ function greet(name,reply="Thanks a lot"){
 greet('Rohan',"Thank you");
 let val = greet("Venky");
 console.log(val);
+
+
+console.log("From function mygreet");
+val = mygreet("Venky");
+console.log(val);
+
+
+
+const myobj = {
+    name: "Learn Overflow",
+    description: function(){
+        return "A YouTube channel"
+    }
+}
+console.log(myobj.name,myobj.description());
