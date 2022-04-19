c4dd8395fc390bcf406beafd6c0446c80cab6b6f
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2022-04-19 21:56:36 +0530

    Understanding DOM & Creating a Website Layout

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,17 @@
 console.log("Understanding Dom and Creating websites");
+
+let a = document;//returns the document object
+console.log(a);
+
+a=document.all;// returns all elements of document
+console.log(a);
+
+Array.from(a).forEach(function(element){ // prints all the elements of the document
+    console.log(element);
+});
+
+a= document.forms;// returns all forms present in website
+console.log(a);
+
+a= document.links;
+console.log(a);
